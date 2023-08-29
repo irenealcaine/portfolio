09cab7e892a7b78b648aa5f39cd4beff9d689932
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-08-29 06:58:39 +0000

    Update 7 files

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
-import MainLayout from '../../Layout/MainLayout/MainLayout'
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import MainLayout from "../../Layout/MainLayout/MainLayout";
+import { useTranslation } from "react-i18next";
+import "./About.scss";
+import { experience } from "../../data/AboutData";
 
 const About = () => {
-
   const { i18n, t } = useTranslation("global");
-  console.log(i18n.language)
+  console.log(i18n.language);
 
   return (
-    <div>
-      <MainLayout>
-        {t('aboutPage.title')}
-      </MainLayout>
-    </div>
-  )
-}
+    <MainLayout>
+      <div className="about">
+        <h1>{t("aboutPage.title")}</h1>
+        {/* <h2>{t("aboutPage.skills")}</h2> */}
+        <h2>{t("aboutPage.experience")}</h2>
+        <ul>
+          {experience.map((exp, index) => (
+            <li>{exp.en.title}</li>
+          ))}
+        </ul>
+        <h2>{t("aboutPage.education")}</h2>
+        <h2>{t("aboutPage.contact")}</h2>
+      </div>
+    </MainLayout>
+  );
+};
 
-export default About
+export default About;
---
+++ src/data/AboutData.js
@@ -0,0 +1,28 @@
+export const experience = [
+  {
+    id: "1",
+    es: {
+      title: "Cajera",
+      date: "Ene 2021 - Jun 2021",
+      description: ["ReactJS", "TailwindCSS"],
+    },
+    en: {
+      title: "Shoper",
+      date: "Jan 2021 - Jun 2021",
+      description: ["ReactJS", "TailwindCSS"],
+    },
+  },
+  {
+    id: "1",
+    es: {
+      title: "Cajera",
+      date: "Ene 2021 - Jun 2021",
+      description: ["ReactJS", "TailwindCSS"],
+    },
+    en: {
+      title: "Shoper",
+      date: "Jan 2021 - Jun 2021",
+      description: ["ReactJS", "TailwindCSS"],
+    },
+  },
+];
