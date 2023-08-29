ca7b1089af2aac8f6a03c4089b5f301d4d963578
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-08-29 06:20:17 +0000

    Update 3 files

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,31 +1,50 @@
-import React from 'react'
-import "./Header.scss"
-import { IoChevronBackOutline } from "react-icons/io5"
-import { AiOutlineMail, AiOutlineCodepen } from "react-icons/ai"
-import { FiGithub } from "react-icons/fi"
+import React, { useContext } from "react";
+import "./Header.scss";
+import { IoChevronBackOutline } from "react-icons/io5";
+import {
+  AiOutlineMail,
+  AiOutlineCodepen,
+  AiOutlinePoweroff,
+} from "react-icons/ai";
+import { FiGithub } from "react-icons/fi";
 import { SlSocialLinkedin } from "react-icons/sl";
-import { Link } from 'react-router-dom'
-import { useTranslation } from 'react-i18next';
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { NeonOnContext } from "../../Context/neonOnContext";
 
 const Header = () => {
   const { i18n, t } = useTranslation("global");
+  const { dispatch } = useContext(NeonOnContext);
 
   const toggleLanguage = () => {
-    i18n.changeLanguage(i18n.language === 'en' ? 'es' : 'en');
+    i18n.changeLanguage(i18n.language === "en" ? "es" : "en");
   };
 
   return (
     <header>
-      <Link to={"/"} className='backButton'><IoChevronBackOutline /></Link>
+      <Link to={"/"} className="backButton">
+        <IoChevronBackOutline />
+      </Link>
       <nav>
-        <div onClick={toggleLanguage}>{t('languageSelector')}</div>
-        <div className='icon'><AiOutlineCodepen /></div>
-        <div className='icon'><FiGithub /></div>
-        <div className='icon'><SlSocialLinkedin /></div>
-        <div className='icon'><AiOutlineMail /></div>
+        <div onClick={toggleLanguage}>{t("languageSelector")}</div>
+        <div className="icon" onClick={() => dispatch({ type: "TOGGLE" })}>
+          <AiOutlinePoweroff />
+        </div>
+        <div className="icon">
+          <AiOutlineCodepen />
+        </div>
+        <div className="icon">
+          <FiGithub />
+        </div>
+        <div className="icon">
+          <SlSocialLinkedin />
+        </div>
+        <div className="icon">
+          <AiOutlineMail />
+        </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
