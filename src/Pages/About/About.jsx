2d1195f311c38189a40e400eef3365a84e445f86
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-08-29 10:17:03 +0000

    Update 3 files

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -14,11 +14,46 @@ const About = () => {
         <h1>{t("aboutPage.title")}</h1>
         {/* <h2>{t("aboutPage.skills")}</h2> */}
         <h2>{t("aboutPage.experience")}</h2>
-        <ul>
-          {experience.map((exp, index) => (
-            <li>{exp.en.title}</li>
-          ))}
-        </ul>
+        <div className="container">
+          <ul className="timeline">
+            {experience.toReversed().map((exp, index) => {
+              return i18n.language === "es" ? (
+                <li
+                  key={index}
+                  className={`${
+                    index % 2 !== 0
+                      ? "timeline-block-right"
+                      : "timeline-block-left"
+                  }`}
+                >
+                  <div class="marker"></div>
+                  <div className="timeline-panel">
+                    <div className="timeline-header">
+                      <h3>{exp.es.title}</h3>
+                      <h4>{exp.en.ubication}</h4>
+                    </div>
+
+                    <div className="timeline-body">
+                      <ul>
+                        {exp.es.description.map((m, index) => (
+                          <li key={index}>{m}</li>
+                        ))}
+                      </ul>
+                    </div>
+                    <div className="timeline-footer">
+                      <p>{exp.es.date}</p>
+                    </div>
+                  </div>
+                </li>
+              ) : (
+                <li key={index}>
+                  <p>{exp.en.title}</p>
+                  <p>{exp.en.description}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
         <h2>{t("aboutPage.education")}</h2>
         <h2>{t("aboutPage.contact")}</h2>
       </div>
--- src/data/AboutData.js
+++ src/data/AboutData.js
@@ -3,24 +3,43 @@ export const experience = [
     id: "1",
     es: {
       title: "Cajera",
+      ubication: "Charter, Valencia, Casinos",
       date: "Ene 2021 - Jun 2021",
       description: ["ReactJS", "TailwindCSS"],
     },
     en: {
       title: "Shoper",
+      ubication: "Charter, Valencia, Casinos",
       date: "Jan 2021 - Jun 2021",
       description: ["ReactJS", "TailwindCSS"],
     },
   },
   {
-    id: "1",
+    id: "2",
     es: {
-      title: "Cajera",
+      title: "Apuestas",
+      ubication: "Charter, Valencia, Casinos",
       date: "Ene 2021 - Jun 2021",
       description: ["ReactJS", "TailwindCSS"],
     },
     en: {
       title: "Shoper",
+      ubication: "Charter, Valencia, Casinos",
+      date: "Jan 2021 - Jun 2021",
+      description: ["ReactJS", "TailwindCSS"],
+    },
+  },
+  {
+    id: "3",
+    es: {
+      title: "Apuestas",
+      ubication: "Charter, Valencia, Casinos",
+      date: "Ene 2021 - Jun 2021",
+      description: ["ReactJS", "TailwindCSS"],
+    },
+    en: {
+      title: "Shoper",
+      ubication: "Charter, Valencia, Casinos",
       date: "Jan 2021 - Jun 2021",
       description: ["ReactJS", "TailwindCSS"],
     },
