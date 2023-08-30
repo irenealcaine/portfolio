e89d759c642e2645e514ab8dad35f0681f420296
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-08-30 11:49:12 +0000

    Update 5 files

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import { AiOutlineMail, AiOutlineCodepen } from "react-icons/ai"
-import { FiGithub } from "react-icons/fi"
+import React, { useContext } from "react";
+import { AiOutlineMail, AiOutlineCodepen } from "react-icons/ai";
+import { FiGithub } from "react-icons/fi";
 import { SlSocialLinkedin } from "react-icons/sl";
-import "./Footer.scss"
+import "./Footer.scss";
+import { NeonOnContext } from "../../Context/neonOnContext";
 
 const Footer = () => {
+  const { neonOn } = useContext(NeonOnContext);
+
   return (
     <footer>
-
       <nav>
-        <div className='icon'><AiOutlineCodepen /></div>
-        <div className='icon'><FiGithub /></div>
-        <div className='icon'><SlSocialLinkedin /></div>
-        <div className='icon'><AiOutlineMail /></div>
+        <div className={`icon purple ${neonOn && "on"}`}>
+          <AiOutlineCodepen />
+        </div>
+        <div className={`icon yellow ${neonOn && "on"}`}>
+          <FiGithub />
+        </div>
+        <div className={`icon lightBlue ${neonOn && "on"}`}>
+          <SlSocialLinkedin />
+        </div>
+        <div className={`icon red ${neonOn && "on"}`}>
+          <AiOutlineMail />
+        </div>
       </nav>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
