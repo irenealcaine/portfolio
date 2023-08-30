4225ef1e62383712aebe0c4c5bb87242615b3ae3
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-08-30 10:11:41 +0000

    Update 3 files

--- src/Components/AboutCard/AboutCard.jsx
+++ src/Components/AboutCard/AboutCard.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import me from "../../Assets/Images/me.png";
+import "./AboutCard.scss";
 
 const AboutCard = () => {
-
   const { t } = useTranslation("global");
 
   return (
-    <div>
-
+    <div className="about-card">
       <h1>Irene Alcaine</h1>
       <h2>Frontend developer</h2>
       {/* <p>{global[lang].about}</p> */}
-      <p>{t('about.presentation')}</p>
-      <p>{t('about.button')}</p>
-
+      <p>{t("about.presentation")}</p>
+      <p>{t("about.button")}</p>
+      <img
+        src={me}
+        // src="https://agcnwo.com/wp-content/uploads/2020/09/avatar-placeholder.png"
+        alt="me"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default AboutCard
+export default AboutCard;
