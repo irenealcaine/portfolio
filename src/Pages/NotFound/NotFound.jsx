b373c47b536e8ab3a1be5b2c122cee86d0cd30b3
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-09-01 10:29:45 +0000

    Update 3 files

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,10 +1,27 @@
 import MainLayout from "../../Layout/MainLayout/MainLayout";
-
+import { useTranslation } from "react-i18next";
 
 const NotFound = () => {
+  const {t } = useTranslation("global");
+
     return (
         <MainLayout>
-            <span className='title'>Ooops</span>
+            <h1>{t("notFoundPage.title")}</h1>
+            <h2>{t("notFoundPage.subtitle")}</h2>
+            <p>{t("notFoundPage.list.header")}</p>
+            <ul>
+                {/* {for(i=1, i<3, i++){
+                    <li><p>{t(`notFoundPage.list.option${i}`)}</p></li>
+                }} */}
+
+{[1, 2].map((i) => (
+    <li key={i}>
+      <p>{t(`notFoundPage.list.option${i}`)}</p>
+    </li>
+  ))}
+                
+                <li><p>{t("notFoundPage.list.option2")}</p></li>
+            </ul>
         </MainLayout>
     
     )
