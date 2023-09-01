6dcf6dcc5088763e5fe6f24c3570ffde041be294
Irene Alcaine <irenealcainealvarez@gmail.com>
2023-09-01 17:55:58 +0200

    update 404

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,34 +1,37 @@
-import React, { useContext} from "react";
+import React, { useContext } from "react";
 import MainLayout from "../../Layout/MainLayout/MainLayout";
 import { useTranslation } from "react-i18next";
 import "./NotFound.scss"
 import { NeonOnContext } from "../../Context/neonOnContext";
+import { Link } from "react-router-dom";
 
 
 const NotFound = () => {
-  const {t } = useTranslation("global");
+  const { t } = useTranslation("global");
   const { neonOn } = useContext(NeonOnContext);
 
 
-    return (
-        <MainLayout>
-<div className="notFound">
-            <h1>{t("notFoundPage.title")}</h1>
-            <h2 className={neonOn && "on"}>{t("notFoundPage.subtitle")}</h2>
-            <p>{t("notFoundPage.list.header")}</p>
-            <ul>
+  return (
+    <MainLayout>
+      <div className="notFound">
+        <h1>{t("notFoundPage.title")}</h1>
+        <h2 className={neonOn && "on"}>{t("notFoundPage.subtitle")}</h2>
+        <div className="message">
+          <p>{t("notFoundPage.list.header")}</p>
+          <ul>
 
-                {[1, 2].map((i) => (
-                  <li key={i}>
-                    <p>{t(`notFoundPage.list.option${i}`)}</p>
-                  </li>
-                ))}
-                
-            </ul>
-</div>
-        </MainLayout>
-    
-    )
+            {[1, 2].map((i) => (
+              <li key={i}>
+                <p>{t(`notFoundPage.list.option${i}`)}</p>
+              </li>
+            ))}
+            <Link to="/" className={neonOn && "on"} >irenealcainealvarez.es</Link>
+          </ul>
+        </div>
+      </div>
+    </MainLayout>
+
+  )
 }
 
 export default NotFound
