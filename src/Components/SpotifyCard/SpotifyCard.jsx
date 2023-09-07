ecfd05450ea7e8315a0a8d54cf6e14d7f7f498ba
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-09-07 08:02:11 +0000

    Update SpotifyCard.jsx

--- src/Components/SpotifyCard/SpotifyCard.jsx
+++ src/Components/SpotifyCard/SpotifyCard.jsx
@@ -1,20 +1,24 @@
 import { useTranslation } from "react-i18next";
 import { SlSocialSpotify } from "react-icons/sl"
-import SpotifyWebApi from "spotify-web-api-node";
 import "./SpotifyCard.scss"
-import { useEffect, useState } from "react";
 import { useLanyard } from "react-use-lanyard";
-
-
-// const spotifyApi = new SpotifyWebApi({
-//   clientId: process.env.CLIENT_ID,
-//   clientSecret: process.env.CLIENT_SECRET,
-//   redirectUri: process.env.REDIRECT_URI
-// });
 
 const SpotifyCard = () => {
 
   const { t } = useTranslation("global");
+
+  const favouriteSongs =[
+    {
+      "song":"Melendi", 
+      "artist":"Caminando por la vida"
+    },{
+      "song":"Estopa", 
+      "artist":"Como Camarón"
+    }
+  ]
+
+  const randomSong = Math.floor(Math.random() * favouriteSongs.length)
+
     const { status } = useLanyard({
       userId: "952910556250136586",
       socket: true,
@@ -22,18 +26,18 @@ const SpotifyCard = () => {
 
   return (
     <a 
-    href={`https://open.spotify.com/track/${status?.spotify?.track_id}` || `https://open.spotify.com/track/6Qi1MNFHCLo4bVsV14MOmz`} 
+    href={`https://open.spotify.com/track/${status?.spotify?.track_id}` || `https://open.spotify.com/playlist/01Fvemoc0peRcK1CoU3ohz`} 
     target="_blank"
     rel="noopener noreferrer" 
     className='spotifyCard'
     >
       <p className="title">{t("aboutPage.spotify.recent")} <span><SlSocialSpotify /> </span></p>
       <h3 className="">
-        {status?.spotify?.song || "Libertad"}
+        {status?.spotify?.song || favouriteSongs[randomSong].song}
       </h3>
       {" - "}
       <p className="">
-        {status?.spotify?.artist || "Nil Moliner"}
+        {status?.spotify?.artist || favouriteSongs[randomSong].artist}
       </p>
       <div>
       </div>
