02a6ed1ed6eefb7309174938162969daba0a841c
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-09-29 07:52:08 +0000

    Update 2 files

--- src/Components/ProjectsCard/ProjectsCard.jsx
+++ src/Components/ProjectsCard/ProjectsCard.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next';
+import React, { useContext } from "react";
+import { NeonOnContext } from "../../Context/neonOnContext";
+import { useTranslation } from "react-i18next";
+import logo from "../../Assets/Images/monsterslogo.png";
+import "./ProjectsCard.scss";
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+import { projects } from "../../data/PortfolioData";
 
 const ProjectsCard = () => {
   const { t } = useTranslation("global");
+  const { neonOn } = useContext(NeonOnContext);
 
   return (
-    <div>
+    <div className="projects-card">
       <h2>{t("projects.title")}</h2>
       <p>{t("projects.description")}</p>
+      <div className="examples">
+        {projects.slice(1, 5).map((project) => (
+          <img
+            src={project.images.icon}
+            className={`project-image ${neonOn && "on"}`}
+            alt={project.es.title}
+          />
+        ))}
+      </div>
+      <p className="button">
+        <span>{t("about.button")}</span>{" "}
+        <MdKeyboardDoubleArrowRight className={`arow ${neonOn && "on"}`} />
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectsCard
+export default ProjectsCard;
